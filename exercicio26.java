018bc62763345296f6684f1087eaf91fc40e7441
RelampagoMinato <julioandreguedes@gmail.com>
2024-03-31 19:59:00 -0300

    Exe26

--- exercicio26.java
+++ exercicio26.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import Dominio.Seguro;
+import java.util.Scanner;
 
 public class exercicio26 {
     public static void executar() {
@@ -25,3 +25,4 @@ public class exercicio26 {
         scanner.close();
     }
 }
+ 
